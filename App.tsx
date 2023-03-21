78bc7ba9989dcbc4bfcc9b3ef5d99e20a7d87323
Nbatsch <112988186+Nbatsch@users.noreply.github.com>
2023-03-20 23:22:08 -0400

    Test deploy 2

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Learn React with Nick</p>
+      <p>Learn React with Nick and MSU!</p>
     </div>
   );
 }
